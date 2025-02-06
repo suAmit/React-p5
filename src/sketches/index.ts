56497f60f11039f278ca391191799b66e58fc629
main <147977973+suAmit@users.noreply.github.com>
2025-02-06 20:08:54 +0000

    fix for theme change bug

--- src/sketches/index.ts
+++ src/sketches/index.ts
@@ -1,3 +1,7 @@
 export const sketchIndex = [
-  { title: "Sketch 1", desc: "Example sketch", filepath: "example" },
+  {
+    title: "Sketch 1",
+    desc: "Example sketch for testing the p5 with react and typescript",
+    filepath: "example",
+  },
 ];
